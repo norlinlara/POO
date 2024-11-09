2ccc34491d041b37ea68fb5bd126333732d02666
norlinlara <nslara@uth.hn>
2024-11-09 15:03:17 -0600

    Actulizacion de la clase  perro

--- src/main/java/POO/Herencia.java
+++ src/main/java/POO/Herencia.java
@@ -4,6 +4,9 @@
 
 package POO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author hi
@@ -14,12 +17,26 @@ public class Herencia {
     public static void main(String[] args) {
         
         Perro p = new Perro( " Color Negro", "Sii", "Si", "Si ",4);
-        p.Sonido();
+        //p.Sonido();
         
         Gato g = new Gato("Blanco", "Si", "Si", "Si", 4);
-        g.Sonido();
+        //g.Sonido();
         
         Queco q = new Queco("No tiene","Si", "Si","Si", 4);
-        q.Sonido();
+        //q.Sonido();
+        
+        List<Animal> lista = new ArrayList<>();
+        lista.add(p);
+        lista.add(g);
+        lista.add(q);
+        lista.add(new Queco("NO", "SI", "SI", "SI", 4));
+        
+        for (Animal item : lista){
+            item.Sonido();
+        
+        
+        }
+                
+        
     }
 }
